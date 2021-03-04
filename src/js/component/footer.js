eb2dc5d198781aa87b8910b21651077c2f7e4da8
Olger Garita Torres <olgeragt@gmail.com>
2021-03-04 21:58:20 +0000

    React with Bootstrap

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer className="page-footer font-small special-color-white mt-4">
+			<div className="footer-copyright text-center pt-40 bg-dark">
+				Copyright © Your Website 2021
+			</div>
+		</footer>
+	);
+}
